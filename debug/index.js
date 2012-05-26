0b1322fecebe3ebbc20a6ec58aecf3df7c4d40f6
David Howell <davidh@dynamicmethods.com.au>
2012-05-27 00:01:48 +0930

    Updated example to test removals

--- debug/index.js
+++ debug/index.js
@@ -6,13 +6,25 @@
 	});
 
 	var adelaide = new L.LatLng(-34.93027490891421, 138.603875041008);
+	var m = new R.Marker(adelaide);
 
 	map.setView(adelaide, 13).addLayer(tiles);
-	map.addLayer(new R.Marker(adelaide));
+	map.addLayer(m);
+
+	setTimeout(function() {
+		map.removeLayer(m);
+	}, 5000);
 
 	map.on('click', function(e) {
-		map.addLayer(new R.Bezier([adelaide, e.latlng], {'stroke': '#f00', 'stroke-width': 3}));
-		console.log(e.latlng);
-		map.addLayer(new R.Pulse(e.latlng, {'stroke': '#2478ad', 'fill': '#30a3ec'}, {'stroke': '#30a3ec', 'stroke-width': 3}));
+		var b = new R.Bezier([adelaide, e.latlng], {'stroke': '#f00', 'stroke-width': 3});
+		var p = new R.Pulse(e.latlng, {'stroke': '#2478ad', 'fill': '#30a3ec'}, {'stroke': '#30a3ec', 'stroke-width': 3});
+
+		map.addLayer(b);
+		map.addLayer(p);
+
+		setTimeout(function() {
+			map.removeLayer(b);
+			map.removeLayer(p);
+		}, 3000);
 	});
 })();
