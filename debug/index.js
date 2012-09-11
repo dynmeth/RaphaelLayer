401bd3790f01f723088ede42421dc6b431df42dd
David Howell <davidh@dynamicmethods.com.au>
2012-09-11 21:04:47 +0930

    Removed console logs from debug demo

--- debug/index.js
+++ debug/index.js
@@ -27,8 +27,7 @@
 
 			},
 			function() {
-				console.log('out');
-				p.animate({opacity: 0}, 1000, function() {map.removeLayer(p); console.log('done') });
+				p.animate({opacity: 0}, 1000, function() { map.removeLayer(p); });
 
 			});
 
