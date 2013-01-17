6f6a85799d207d7c244ab92a29ba18e2a0c6c525
David Howell <davidh@dynamicmethods.com.au>
2013-01-17 23:09:36 +1030

    Added copyright.js

---
+++ src/copyright.js
@@ -0,0 +1,5 @@
+/*
+ RaphaelLayer, a JavaScript library for overlaying Raphael objects onto Leaflet interactive maps. http://dynmeth.github.com/RaphaelLayer
+ (c) 2012-2013, David Howell, Dynamic Methods Pty Ltd
+*/
+
